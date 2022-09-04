5e406058aa9136a269bc8475fa14d84467c6dfde
Sean-Miningah <miningahsean@gmail.com>
2022-09-04 13:23:23 +0300

    Technician admin dashboard

--- technician/admin.py
+++ technician/admin.py
@@ -1,3 +1,89 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (TechnicianDetails, Specialization, TechnicianSpecializations, 
+        ShopFeedbackRating, SkillBadge, TechnicianBadge)
+
+
+class TechnicianDetailsConfig(admin.ModelAdmin):
+    search_fields = ('autouser',)
+    list_display = ('rating', 'shop_goal', 'lat', 'long', 'id')
+
+    fieldsets = (
+        (None, {'fields': ('autouser', 'profile_picture', 'rating')}),
+        ('Location', {'fields': ('lat', 'lng')}),
+        ('Description', {'fields': ('shop_description', 'shop_goal')})
+    )
+
+    add_fieldsets = (
+        (None, {'fields': ('autouser', 'profile_picture', 'rating')}),
+        ('Location', {'fields': ('lat', 'lng')}),
+        ('Description', {'fields': ('shop_description', 'shop_goal')})
+    )
+
+class SpecializationConfig(admin.ModelAdmin):
+    list_display=('id', 'name',)
+
+    fieldsets = (
+        (None, {'fields': ('name',)}),
+    )
+
+    add_fieldsets = (
+        (None, {'fields': ('name',)}),
+    )
+
+class TechnicianSpecializationsConfig(admin.ModelAdmin):
+    search_fields = ('technician','specialization',)
+    list_display = ('technician', 'specialization')
+
+    fieldsets = (
+        (None, {'fields': ('technician', 'specialization')}),
+    )
+
+    add_fieldsets = (
+        (None, {'fields': ('specialization', 'technician')}),
+    )
+
+class ShopFeedbackRatingConfig(admin.ModelAdmin):
+    search_fields = ('technician', 'autouser')
+    list_display = ('id', 'rating', 'description')
+
+    fieldsets = (
+        (None, {'fields': ('description', 'rating')}),
+        ('Stakeholders', {'fields': ('technician', 'autouser')})
+    )
+
+    add_fieldsets = (
+        (None, {'fields': ('description', 'rating')}),
+        ('Stakeholders', {'fields': ('technician', 'autouser')})
+    )
+
+class SkillBadgeConfig(admin.ModelAdmin):
+    search_fields = ('badge',)
+    list_display = ('id', 'badge')
+
+    fieldsets = (
+        (None, {'fields': ('badge',)}),
+    )
+
+    add_fieldsets = (
+        (None, {'fields': ('badge',)}),
+    )
+
+class TechnicianBadgeConfig(admin.ModelAdmin):
+    search_fields = ('badge', 'technician')
+    list_display = ('id', 'badge', 'technician')
+
+    fieldsets = (
+        (None, {'fields': ('badge', 'technician')}),
+    )
+
+    add_fieldsets = (
+        (None, {'fields': ('badge', 'technician')}),
+    )
+
+admin.site.register(TechnicianDetails, TechnicianDetailsConfig)
+admin.site.register(Specialization, SpecializationConfig)
+admin.site.register(TechnicianSpecializations, TechnicianSpecializationsConfig)
+admin.site.register(ShopFeedbackRating, ShopFeedbackRatingConfig)
+admin.site.register(SkillBadge, SkillBadgeConfig)
+admin.site.register(TechnicianBadge, TechnicianBadgeConfig)
